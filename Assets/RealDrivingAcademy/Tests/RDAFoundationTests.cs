88546408b6fe7886c8f42221736368abc7dc01c3
nmg200101-collab <nmg200101@gmail.com>
2026-09-07 21:15:50 +0300

    RDA V50: add foundation edit mode tests

---
+++ Assets/RealDrivingAcademy/Tests/RDAFoundationTests.cs
@@ -0,0 +1,29 @@
+#if UNITY_EDITOR
+using NUnit.Framework;
+using RDA.V50.Academy;
+using RDA.V50.Save;
+
+namespace RDA.V50.Tests
+{
+    public sealed class RDAFoundationTests
+    {
+        [Test]
+        public void NewProgress_HasExpectedDefaults()
+        {
+            var p = new RDAPlayerProgress();
+            Assert.AreEqual(1, p.schemaVersion);
+            Assert.IsTrue(p.guest);
+            Assert.AreEqual(0, p.totalScore);
+        }
+
+        [Test]
+        public void Evaluation_DefaultsAreSafe()
+        {
+            var state = new RDAEvaluationState();
+            Assert.AreEqual(100, state.score);
+            Assert.AreEqual(0, state.faults);
+            Assert.IsFalse(state.failed);
+        }
+    }
+}
+#endif
